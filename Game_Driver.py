9c921bcf556384a124b02f475c5dde2e8429da90
Jeffrey Deng <jeffreytheflash123@gmail.com>
2019-07-22 21:21:23 -0400

    I play pokemon everyday

--- Game_Driver.py
+++ Game_Driver.py
@@ -1,3 +1,15 @@
 import pygame
 from pygame.locals import *
 
+SCREENSIZE_X = 1000
+SCREENSIZE_Y = 618
+
+def main():
+    screen = pygame.display.set_mode((1024, 768))
+    pygame.display.set_caption('Battle Simulation')
+    screen.fill((255, 255, 255))
+    background = pygame.image.load("background.png")
+    screen.blit(background, (250, 250))
+    pygame.display.update()
+
+main()
