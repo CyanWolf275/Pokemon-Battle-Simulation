d04caf4b868a06f0adf24e22dc56b2e073871244
jjdow75 <13918831764@163.com>
2019-07-22 21:20:07 -0400

    connect

--- tcp_client.py
+++ tcp_client.py
@@ -4,4 +4,12 @@ class client(object):
 
     def __init__(self, ip, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((ip, port))
+        self.info = (ip, port)
+    
+    def connect(self):
+        '''Connect to a server. Returns True if succeeded, otherwise False. '''
+        try:
+            s.connect(self.info)
+            return True
+        except:
+            return False
