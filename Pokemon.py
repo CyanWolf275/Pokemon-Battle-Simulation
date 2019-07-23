1842f025bca4b8ff96e178a7c44d279288c91711
Ding_Keyi <53192239+RussellDKY@users.noreply.github.com>
2019-07-23 15:56:31 -0400

    Add files via upload

---
+++ Pokemon.py
@@ -0,0 +1,23 @@
+class Pokemon:
+    
+    #attributes:
+    #name
+    #attack
+    #defense
+    #SpAttack
+    #SpDefence
+    #Speed
+    #FrontPic
+    #BackPic
+    
+    def __init__(self,name,attack,defense,SpAttack,SpDefence,Speed,FrontPic,BackPic):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.SpAttack = SpAttack
+        self.SpDefence = SpDefence
+        self.Speed = Speed
+        self.FrontPic = FrontPic
+        self.BackPIc = BackPic
+        
+    
