8e85da14336c9632ea494955394f53b691f540b1
jjdow75 <13918831764@163.com>
2019-07-23 06:21:10 -0400

    tcp

--- tcp_server.py
+++ tcp_server.py
@@ -1,15 +1,18 @@
-import socket
+import socket, pyodbc
 
 class server(object):
     
     def __init__(self, ip, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.info = (ip, port)
-        s.bind(self.info)
+        self.move_db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ=C:\Users\13918\Documents\Summer\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;').cursor()
+        self.s.bind(self.info)
     
     def connect(self):
         '''Wait until a connection is established and returns the client's ip address. '''
         s.listen(1)
         return s.accept()[0]
-    
-    
+
+    def act(self, pkmn, move):
+        #self.move_db.execute("select * from Status where EnName = '" + move + "'")
+        pass
