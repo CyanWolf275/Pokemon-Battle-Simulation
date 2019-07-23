caccdf2f2d8c9ca4737bf97db422a62601624f33
jjdow75 <13918831764@163.com>
2019-07-23 19:48:28 -0400

    pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -2,25 +2,13 @@ class Pokemon:
     
     #attributes:
     #name
-<<<<<<< HEAD
-    #attack
-    #defense
-=======
     #Attack
     #Defense
->>>>>>> bbd7f05c948b964b97136f763917aba2fce7bf92
     #SpAttack
     #SpDefence
     #Speed
     #FrontPic
     #BackPic
-<<<<<<< HEAD
-    
-    def __init__(self,name,attack,defense,SpAttack,SpDefence,Speed,FrontPic,BackPic):
-        self.name = name
-        self.attack = attack
-        self.defense = defense
-=======
     #level
     #critical
     #ability
@@ -29,17 +17,11 @@ class Pokemon:
         self.name = name
         self.Attack = Attack
         self.Defense = Defense
->>>>>>> bbd7f05c948b964b97136f763917aba2fce7bf92
         self.SpAttack = SpAttack
         self.SpDefence = SpDefence
         self.Speed = Speed
         self.FrontPic = FrontPic
         self.BackPIc = BackPic
-<<<<<<< HEAD
-        
-    
-=======
         self.level = level
         self.critical = critical
         self.ability = ability
->>>>>>> bbd7f05c948b964b97136f763917aba2fce7bf92
