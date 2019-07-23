5a88b904cf6e5468dcf65ddf8e666a844c78311b
jjdow75 <13918831764@163.com>
2019-07-23 13:54:17 -0400

    scrapped available moves

--- scrap.py
+++ scrap.py
@@ -1,7 +1,33 @@
 from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
 import requests, pyodbc
-db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ=C:\Users\13918\Documents\Summer\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;').cursor()
-r = requests.get("https://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)/Generation_VI_learnset#By_leveling_up")
-soup = BeautifulSoup(r.text, 'html.parser')
-trs = soup.findall("tr")
+import lxml.html as lh
+import pandas as pd
+db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ=C:\Users\13918\Documents\Summer\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;')
+cursor = db.cursor()
+cursor.execute("select EnName from BaseStat")
+pkmn = cursor.fetchall()
+i = 1
+for p in pkmn:
+    try:
+        r = requests.get("https://pokemondb.net/pokedex/" + str(p[0]).lower() + "/moves/6")
+        soup = BeautifulSoup(r.text, 'html.parser')
+        tab = soup.find_all("table")[0]
+        table = tab.find_all("tr")
+        lst = {"a"}
+        lst.remove("a")
+        for row in table:
+            cells = row.find_all("td")
+            if len(cells) >= 2:
+                lst.add(cells[1].get_text())
+        output = ""
+        for item in lst:
+            output += item + ", "
+        cursor.execute("update Moves set Move = '" + output[:-2] + "' where EnName = '" + str(p[0]) + "'")
+        db.commit()
+        print(i)
+        i += 1
+    except:
+        print(str(p[0]) + "omitted " + str(i))
+        i += 1
+cursor.close()
+db.close()
