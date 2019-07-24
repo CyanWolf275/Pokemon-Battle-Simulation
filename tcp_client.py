cdb968da2ffbc30649795f9c3dea40bb956148a4
jjdow75 <13918831764@163.com>
2019-07-24 10:33:19 -0400

    tcp

--- tcp_client.py
+++ tcp_client.py
@@ -1,17 +1,56 @@
-import socket, pyodbc, functions
+import socket, pyodbc, functions, time, sys
+import multiprocessing
 
 class client(object):
 
     def __init__(self, ip, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.move_db = pyodbc.connect(
-            r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ= \\asset\database\BaseStatus.accdb;').cursor()
+        #self.recv_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.move_db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ= \\asset\database\BaseStatus.accdb;').cursor()
         self.info = (ip, port)
+        self.msg_recv = ""
+        self.receving = True
     
     def connect(self):
         '''Connect to a server. Returns True if succeeded, otherwise False. '''
         try:
             self.s.connect(self.info)
+            #self.recv_s.connect(self.info)
+            self.recv_thd = multiprocessing.Process(target=self.b_recv)
+            self.recv_thd.start()
             return True
         except:
             return False
+    
+    def send(self, msg):
+        self.s.send(msg.encode())
+    
+    def reeive(self):
+        return self.s.recv(1024).decode()
+    
+    def b_recv(self):
+        while self.receving:
+            self.msg_recv = self.s.recv(1024).decode()
+    
+    def get_msg(self):
+        temp = self.msg_recv[:]
+        self.msg_recv = ""
+        return temp
+    
+    def press_start(self):
+        self.send("start")
+        while not self.get_msg() == "start":
+            time.sleep(0.1)
+        
+
+def main():
+    c = client("127.0.0.1", 25792)
+    c.connect()
+    input("Press Start")
+    c.press_start()
+    print("Entered")
+    c.recv_thd.terminate()
+    c.s.close()
+
+main()
+print("exit")
--- tcp_server.py
+++ tcp_server.py
@@ -1,19 +1,26 @@
-import socket, pyodbc, functions
+import socket, pyodbc, functions, time, sys
 from random import random
+import multiprocessing
 
 class server(object):
     
     def __init__(self, ip, port):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.info = (ip, port)
-        self.move_db = pyodbc.connect( r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};' r'DBQ=C:\Users\jeffr\OneDrive\Desktop\VS2020\BattleSimulation\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;').cursor()
+        #self.move_db = pyodbc.connect( r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};' r'DBQ=C:\Users\jeffr\OneDrive\Desktop\VS2020\BattleSimulation\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;').cursor()
         self.s.bind(self.info)
         self.conn = None
+        #self.recv_conn = None
+        self.msg_recv = ""
+        self.receiving = True
     
     def connect(self):
         '''Wait until a connection is established and returns the client's ip address. '''
         self.s.listen(1)
         self.conn, ip = self.s.accept()
+        #self.recv_conn, ip = self.s.accept()
+        self.recv_thd = multiprocessing.Process(target=self.b_recv)
+        self.recv_thd.start()
         return ip
 
     def act(self, pkmn, mv):
@@ -30,7 +37,36 @@ class server(object):
         data = conn.recv().split(", ")
         return {"hp": data[0], "status": data[1]}
 
-s = server("127.0.0.1", 1000)
-print(s.connect())
-s.conn.sendall("123")
+    def send(self, msg):
+        self.conn.send(msg.encode())
+    
+    def receive(self):
+        return self.conn.recv(1024).decode()
+    
+    def b_recv(self):
+        while self.receive:
+            self.msg_recv = self.recv_conn.recv(1024).decode()
+    
+    def get_msg(self):
+        temp = self.msg_recv[:]
+        self.msg_recv = ""
+        return temp
+    
+    def press_start(self):
+        self.send("start")
+        while not self.get_msg() == "start":
+            time.sleep(0.1)
 
+def main():
+    s = server("127.0.0.1", 25792)
+    print(s.connect())
+    input("Press Start")
+    s.press_start()
+    print("Entered")
+    s.recv_thd.terminate()
+    #s.recv_conn.close()
+    s.conn.close()
+    s.s.close()
+
+main()
+print("exit")
