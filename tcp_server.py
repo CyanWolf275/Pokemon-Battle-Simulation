8194d4a13a6ce0d787e12e4cb6e24f05209e5827
jjdow75 <13918831764@163.com>
2019-07-23 22:12:51 -0400

    multiple changes

--- tcp_server.py
+++ tcp_server.py
@@ -1,4 +1,5 @@
 import socket, pyodbc, functions
+from random import random
 
 class server(object):
     
@@ -7,12 +8,27 @@ class server(object):
         self.info = (ip, port)
         self.move_db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ=C:\Users\13918\Documents\Summer\Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;').cursor()
         self.s.bind(self.info)
+        self.conn = None
     
     def connect(self):
         '''Wait until a connection is established and returns the client's ip address. '''
         s.listen(1)
-        return s.accept()[0]
+        self.conn, ip = s.accept()
+        return ip
 
-    def act(self, pkmn, move):
-        #self.move_db.execute("select * from Status where EnName = '" + move + "'")
-        pass
+    def act(self, pkmn, mv):
+        '''msg: name, atk_cat, atk, pwr, op_code/miss
+        data: hp, status
+        '''
+        exec(mv.my_code)
+        msg = ""
+        if random() >= mv.acc:
+            msg = "miss"
+        else:
+            msg = mv.name + ", " + mv.cat + ", " + str(mv.atk(pkmn)) + ", " + mv.pwr + mv.op_code
+        self.conn.send(msg)
+        data = conn.recv().split(", ")
+        return {"hp": data[0], "status": data[1]}
+
+
+
