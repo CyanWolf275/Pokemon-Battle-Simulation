ea725272cd54c800e55fe1c5f2d920be08ae9b96
jjdow75 <13918831764@163.com>
2019-07-25 00:50:19 -0400

    functions.poke_lst

--- functions.py
+++ functions.py
@@ -113,7 +113,7 @@ def poke_lst():
     db = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'r'DBQ=Pokemon-Battle-Simulation\asset\database\BaseStatus.accdb;')
     cursor = db.cursor()
     cursor.execute("select EnName from BaseStat where ID < 152")
-    result = list(cursor)
+    result = [item[0] for item in list(cursor)]
     cursor.close()
     db.close()
     return result
